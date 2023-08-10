7d98786f0907dcb4f5334cfe456a8e9c5c3a4eb5
mwangiins <mwangicollins@gmail.com>
2023-08-10 15:00:11 -0700

    hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line
+ * main - a C program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line
+ *
  * Return: 0 (Success)
  */
-int main(void)
+init main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
