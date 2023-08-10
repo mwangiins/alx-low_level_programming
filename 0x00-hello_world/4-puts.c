395931fe5363ff5357f7f2fdc4acebf365bf7405
mwangiins <mwangicollins@gmail.com>
2023-08-10 14:12:40 -0700

    Happy coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - main function
- *
- * Retutn: Always 0 (Success)
+ * main - a C program that prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: 0 (Success)
  */
-
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
