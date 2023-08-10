757a78db5d02337ddc804bae791ae5195e1e8812
mwangiins <mwangicollins@gmail.com>
2023-08-10 15:07:24 -0700

    coding is magic

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - a C program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line
+ * main - A program that prints
  *
  * Return: 0 (Success)
  */
-init main(void)
+int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
