a9e446d7786ffd77eef54a6b52c33ab4ebce6461
mwangiins <mwangicollins@gmail.com>
2023-08-10 14:07:46 -0700

    Happy coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - entry point
- * Return: Always 0 (Success)
+ * main - main function
+ *
+ * Retutn: Always 0 (Success)
  */
+
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}	
