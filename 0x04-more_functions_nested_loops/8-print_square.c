cc0d08c019fb9d89cd07d2caf117dd7da7cd2812
mwangiins <mwangicollins195@gmail.com>
2023-08-17 13:13:05 -0700

    So much sunshine

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_square -  a function that prints a square
+* @size: variable
+*
+*/
+void print_square(int size)
+{	
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int c, d;
+for (c = 0; c < size; c++)
+{
+for (d = 0; d < size; d++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
