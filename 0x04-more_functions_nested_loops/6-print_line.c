c1b460f808e45751467d971a5a6e66bf75320e61
mwangiins <mwangicollins195@gmail.com>
2023-08-17 11:21:10 -0700

    The shortest distance

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_line - a function that draws a straight line in the terminal
+* @n: variable
+*
+*/
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int c;
+for (c = 0; c <= n; c++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
