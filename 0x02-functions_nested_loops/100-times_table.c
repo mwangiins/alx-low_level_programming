ba690c59d676b2f23401c573180c6f5235c214d8
mwangiins <mwangicollins195@gmail.com>
2023-08-18 07:52:25 -0700

    The world

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,50 @@
 #include "main.h"
 
 /**
- * print_times_table - prints
- * @n: number of the times table
- *Return: 0
- */
-void times_table(int n)
+* print_times_table - a function prints the n times table, starting with 0
+* @n: variable
+*/
+void print_times_table(int n)
 {
-	int x, y, z;
+int a, b, c;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (x = 0; x <= n; x++)
-		{
-			for (y = 0; y <= n; y++)
-			{
-				z = y * x;
-				if (y == 0)
-				{
-					_putchar(x + '0');
-				} else if (z < 10 && y != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(z + '0');
-				} else if (z >= 10 && z < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((z / 10) + '0');
-					_putchar((z % 10) + '0');
-				} else if (z >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-}	}
+if (n >= 0 && n <= 15)
+{
+for (a = 0; a <= n; a++)
+{
+for (b = 0; b <= n; b++)
+{
+c = b * a;
+if (b == 0)
+{
+_putchar(c + '0');
+}
+else if (c < 10 && b != 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(c + '0');
+}
+else if (c >= 10 && c < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+else if (c >= 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar((c / 100) + '0');
+_putchar(((c / 10) % 10) + '0');
+_putchar((c % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
