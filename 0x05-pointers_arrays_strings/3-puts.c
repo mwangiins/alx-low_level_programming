a9babf26d3dbab1c03b1f424fafccbec530971bd
mwangiins <mwangicollins195@gmail.com>
2023-08-21 14:44:11 -0700

    Fear of lack of computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * _puts - a function that prints a string, followed by a new line, to stdout
- * @str: pointer
- */
+* _puts - a function that prints a string, followed by a new line, to stdout
+* @str: pointer
+*/
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		putchar(*str++);
-	}
-	putchar('\n');
+while (*str != '\0')
+{
+_putchar(*str++);
+}
+_putchar('\n');
 }
