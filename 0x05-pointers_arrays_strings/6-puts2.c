d92c9ee30f35d8325e871082d31e3a72fc9de9d9
mwangiins <mwangicollins195@gmail.com>
2023-08-21 11:58:56 -0700

    Prints every other character of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,29 +1,29 @@
 #include "main.h"
 /**
- * puts2 - function should print only one character out of two
- * starting with the first one
- * @str: input
- * Return: print
- */
+* puts2 - function should print only one character out of two
+* starting with the first one
+* @str: input
+* Return: print
+*/
 void puts2(char *str)
 {
-	int let = 0;
-	int b = 0;
-	char *q = str;
-	int r;
+int let = 0;
+int b = 0;
+char *q = str;
+int r;
 
-	while (*q != '\0')
-	{
-		q++;
-		let++;
-	}
-	b = let - 1;
-	for (r = 0 ; r <= b ; r++)
-	{
-	if (r % 2 == 0)
-	{
-		putchar(str[r]);
-	}
-	}
-	putchar('\n');
+while (*q != '\0')
+{
+q++;
+let++;
+}
+b = let - 1;
+for (r = 0 ; r <= b ; r++)
+{
+if (r % 2 == 0)
+{
+_putchar(str[r]);
+}
+}
+_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -12,5 +13,6 @@ void print_rev(char *s);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void puts_half(char *str);
 
 #endif
