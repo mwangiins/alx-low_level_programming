86720d824e5fd40299d7fe43b46db8bdd640f31b
mwangiins <mwangicollins195@.com>
2023-08-23 18:57:24 +0000

    An array of integers

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,13 +8,13 @@
 
 void reverse_array(int *a, int n)
 {
-	int b;
-	int c;
+        int r;
+        int s;
 
-	for (b = 0; b < n--; b++)
-	{
-		c = a[b];
-		a[b] = a [n];
-		a[n] = c;
-	}
+        for (r = 0; r < (n / 2); r++)
+        {
+                s = a[r];
+                a[r] = a[n - r - 1];
+                a[n - r - 1] = s;
+        }
 }
