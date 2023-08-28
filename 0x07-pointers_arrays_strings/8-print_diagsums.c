2a62feab1f766c385a4b4c694410f3e00865d2cf
mwangiins <mwangicollins195@.com>
2023-08-28 22:07:38 +0000

    Diagonals of a square

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - a function that prints the sum of the two diagonals of a square matrix of integers
+ * print_diagsums -  a function that prints the sum of the two diagonals of a square matrix of integers
  * @a: pointer
  * @size: size
  */
 void print_diagsums(int *a, int size)
 {
-	int x, sum1 = 0, sum2 = 0;
+	int r, sum1 = 0, sum2 = 0;
 
-	for (x = 0; x < size; x++)
+	for (r = 0; r < size; r++)
 	{
-		sum1 += a[x];
+		sum1 += a[r];
 		a += size;
 	}
 
 	a -= size;
 
-	for (x = 0; x < size; x++)
+	for (r = 0; r < size; r++)
 	{
-		sum2 += a[x];
+		sum2 += a[r];
 		a -= size;
 	}
 
