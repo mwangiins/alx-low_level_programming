e9cb81a988b9256f139a2d5e16b84c439f2c79ae
mwangiins <mwangicollins195@.com>
2023-09-01 17:01:37 +0000

    Arguments in programming

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints its name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc * 0]);
+
+	return (0);
+}
