f9c422132d6ec81ce34f7851a7a47db33e334155
mwangiins <mwangicollins195@.com>
2023-09-07 14:19:32 +0000

    The returned pointer

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,5 @@
+#include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
 * string_nconcat - a function that concatenates two strings
 * @s1: first string
@@ -7,35 +7,29 @@
 * @n: number of bytes
 * Return: NULL If the function fails
 */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *con;
-unsigned int len1, len2, i, j;
+unsigned int pos, i;
+i = 0;
+pos = n;
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-
 for (i = 0; s1[i]; i++)
-len1++;
-
-for (i = 0; s2[i] && i < n; i++)
-len2++;
-
-con = malloc(sizeof(char) * (len1 + len2 + 1));
+pos++;
+con = malloc(sizeof(char) * (pos + 1));
 if (con == NULL)
 return (NULL);
+pos = 0;
 
 for (i = 0; s1[i]; i++)
-con[i] = s1[i];
-
-for (j = 0; s2[j] && j < n; j++)
-con[i + j] = s2[j];
-
-con[i + j] = '\0';
+con[pos++] = s1[i];
+for (i = 0; s2[i] && i < n; i++)
+con[pos++] = s2[i];
+con[pos] = '\0';
 
 return (con);
 }
-
