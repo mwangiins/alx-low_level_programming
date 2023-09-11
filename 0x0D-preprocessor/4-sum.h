a694b21d22c1ead400518ae6c7e35424a3a46c72
mwangiins <mwangicollins195@.com>
2023-09-11 11:11:22 +0000

    The sum of numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: defines a a function-like macro SUM(x, y) and computes sum of x and y
+ *
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
