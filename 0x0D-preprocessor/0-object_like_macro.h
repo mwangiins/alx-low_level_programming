600ed31190282b8987fa58cefc364117e1388b61
mwangiins <mwangicollins195@.com>
2023-09-11 10:01:30 +0000

    A macro named size

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ * Desc: header file that defines a macro named SIZE
+ */
+
+#define SIZE 1024
+
+#endif
