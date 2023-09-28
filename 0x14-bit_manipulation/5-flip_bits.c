f1d133b3d9a2a76635d8e4c114c10cb762e19be4
mwangiins <mwangicollins195@gmail.com>
2023-09-28 21:57:09 +0000

    Number of bits flipped

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits -  returns the number of bits you would need to flip
+ * @n: first unsigned long int
+ * @m: second unsigned long int
+ *
+ * Return: number of bits flipped
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int find = 0;
+	unsigned long int res = n ^ m;
+
+	while (res > 0)
+	{
+		find += res & 1;
+		res >>= 1;
+	}
+	return find;
+}
