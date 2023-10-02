963ea33462132574334cf687e1b18c702d291a1c
mwangiins <mwangicollins195@gmail.com>
2023-10-02 15:33:50 +0000

    Prints to the standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 		return (0);
 
@@ -30,8 +29,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 	bytr = read(fd, buffer, letters);
-	if (bytr == -1)
 	close(fd);
+	if (bytr == -1)
 	{
 		free(buffer);
 		return (0);
