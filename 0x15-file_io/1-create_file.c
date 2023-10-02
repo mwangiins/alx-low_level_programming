d421f25462eca394b5e1e5f3920934eede826458
mwangiins <mwangicollins195@gmail.com>
2023-10-02 18:59:29 +0000

    Creating a text file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,18 +13,16 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == 0)
 		return (-1);
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (fd == -1)
-		return (-1);
+	bytw = write(fd, text_content, length);
 
-	if (text_content == NULL)
-		text_content = "";
-
-	for (bytw = 0; text_content[bytw]; bytw++)
-	length = write(fd, text_content, bytw);
-
-	if (length == -1)
+	if (fd == -1 || bytw == -1)
 		return (-1);
 	close(fd);
 
